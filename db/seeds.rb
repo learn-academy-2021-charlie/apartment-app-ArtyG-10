67c07b446778655206e1f1010f7407de5a30db71
ArtyG-10 <a.gourent@outlook.com>
2021-08-31 12:16:55 -0400

    set up basic rails app, schema, and seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  {
+    email: 'johnsmith@test.com',
+    password: '123password',
+    password_confirmation: '123password'
+  }
+]
+
+users.each do |attribute|
+  User.create attribute
+end
+
+apartments = [
+  {
+    street: '1st street',
+    city: 'Dallas',
+    state: 'Texas',
+    manager: 'Joe',
+    email: 'Joe@test.com',
+    price: '1000',
+    bedrooms: 2,
+    bathrooms: 2,
+    pets: 'No pets'
+  },
+  {
+    street: '2nd street',
+    city: 'San Antonio',
+    state: 'Texas',
+    manager: 'Jane',
+    email: 'Jane@test.com',
+    price: '700',
+    bedrooms: 1,
+    bathrooms: 1,
+    pets: '1 cat'
+  }
+]
+
+first_user = User.first
+# first_user = User.where(email: 'johnsmith@test.com').first
+
+apartments.each do |key|
+  first_user.apartments.create key
+end
