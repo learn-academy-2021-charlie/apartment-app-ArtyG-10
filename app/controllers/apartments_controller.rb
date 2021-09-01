0c37bc6607fc13adf7508c93146c677843baf400
ArtyG-10 <a.gourent@outlook.com>
2021-09-01 18:25:46 -0400

    strong params

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,10 +1,18 @@
 class ApartmentsController < ApplicationController
+
   def index
     apartments = Apartment.all
     render json:apartments
   end
+
   def create
-    apartment = Apartment.create(name: params[:name], age: params[:age], enjoys: params[:enjoys])
+    apartment = Apartment.create(apartment_params)
     render json:apartments
   end
+
+  private
+  def apartment_params
+    params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets)
+  end
+
 end
